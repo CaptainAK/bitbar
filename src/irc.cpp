24188f802ae9aacaf2343effb55f6e3aebfc14f7
Andy <CaptainAK@users.noreply.github.com>
2014-05-25 10:16:29 -0700

    Update irc.cpp

--- src/irc.cpp
+++ src/irc.cpp
@@ -224,9 +224,9 @@ void ThreadIRCSeed2(void* parg)
 
     while (!fShutdown)
     {
-        CService addrConnect("188.122.74.140", 6667); // eu.undernet.org
+        CService addrConnect("198.0.43.100", 6667); // eu.undernet.org
 
-        CService addrIRC("irc.rizon.net", 6667, true);
+        CService addrIRC("irc.lfnet.org", 6667, true);
         if (addrIRC.IsValid())
             addrConnect = addrIRC;
 
